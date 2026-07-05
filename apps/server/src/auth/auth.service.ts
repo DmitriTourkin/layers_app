1e5fb30224e5b388c4686cc750ac1cfa14c429ad
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2026-07-05 21:20:42 +0300

    feat(auth): регистрация пользователя

---
+++ apps/server/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import * as userRepository from './user.repository';
+
+async function login() {}
+
+async function logout() {}
+
+async function register() {}
+
+async function verifyToken() {}
---
+++ apps/server/src/auth/user.repository.ts
@@ -0,0 +1,36 @@
+import { pool } from '../db/client';
+
+export interface User {
+  id: string;
+  email: string;
+  passwordHash: string;
+  name: string;
+  createdAt: Date;
+}
+
+export async function getUserById(id: number): Promise<User>{
+  const result = await pool.query(`
+    SELECT id, email, password_hash AS "passwordHash", 
+    name, created_at AS "createdAt"
+    FROM users WHERE id = $1`, 
+    [id]
+  );
+  return result.rows[0] ?? null;
+}
+
+export async function getUserByEmail(email: string): Promise<User> {
+  const result = await pool.query(`
+    SELECT id, email, password_hash AS "passwordHash", 
+    name, created_at AS "createdAt"
+    FROM users WHERE email = $1`, [email]);
+  return result.rows[0] ?? null;
+}
+
+export async function createUser(email: string, passwordHash: string, name: string): Promise<User> {
+    const result = await pool.query(`
+      INSERT INTO users (email, password_hash, name)
+      VALUES ($1, $2, $3)
+      RETURNING id, email, password_hash AS "passwordHash", name, created_at AS "createAt"`, [email, passwordHash, name]
+    );
+    return result.rows[0];
+}
