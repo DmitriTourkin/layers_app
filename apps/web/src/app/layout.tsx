dcaae0155997e6dc25ff9f72521ba2a5c48a1f7f
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2026-07-25 23:59:05 +0300

    front(app): добавить провайдер контекста

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Providers } from "@/store/Providers";
 
 export default function RootLayout({
   children,
@@ -6,8 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ru">
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
